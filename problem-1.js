62281edd4be90fcacf6525b5f7dad2b745d41304
Ian Ames <iames@riotgames.com>
2014-10-27 19:35:24 -0700

    initial problem

---
+++ problem-1.js
@@ -0,0 +1,11 @@
+function problemOne () {
+  console.log('Problem 1');
+  for(var i = 0; i < 1000; i++) {
+    var sum = 0;
+
+    if(i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    };
+  };
+  console.log('sum: ', sum);
+};
